3cdae22d1eebfe56b1e459a8889cfea2b9cb0088
Rohit Thange <rohit.a.thange@outlook.com>
2024-03-21 20:24:34 +0530

    Logging and Exception files added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
-    get_requirements=[]
+    requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
